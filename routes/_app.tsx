ac6274b6e701948b1f97fd07968187856f2dc842
Sophie Atkins <atkins@prisma.io>
2023-12-30 00:28:09 +0100

    init w/ Fresh 🍋 🚀

---
+++ routes/_app.tsx
@@ -0,0 +1,18 @@
+import { type PageProps } from "$fresh/server.ts";
+import { JSX } from "preact/jsx-runtime";
+
+export default function App({ Component }: PageProps): JSX.Element {
+  return (
+    <html>
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>rps</title>
+        <link rel="stylesheet" href="/styles.css" />
+      </head>
+      <body>
+        <Component />
+      </body>
+    </html>
+  );
+}
