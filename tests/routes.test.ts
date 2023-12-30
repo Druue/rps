d28e43da400db3002f5c8a989ecc08794b0ea31f
Sophie Atkins <atkins@prisma.io>
2023-12-30 01:03:11 +0100

    Add testing dir with example test

---
+++ tests/routes.test.ts
@@ -0,0 +1,7 @@
+import { assertEquals } from "$std/assert/mod.ts";
+
+// TODO(@druue) example test, replace with actual tests
+Deno.test("Hi", () => {
+  const x = 1 + 2;
+  assertEquals(x, 3);
+});
